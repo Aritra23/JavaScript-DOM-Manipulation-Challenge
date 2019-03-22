0700e98156455850668364ee1da3274281d19266
Aritra Kumar Lahiri <lahiri.aritra@gmail.com>
2019-03-22 15:50:08 -0700

    Add code for populating table data

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Selectors initialization
+var filterButton = d3.select("#filter-btn");
+var dateFilter = d3.select("#date");
+var cityFilter = d3.select("#city");
+var stateFilter = d3.select("#state");
+
+var tbody = d3.select("tbody");
+var resetButton = d3.select("#reset-btn");
+var dataList = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+//Function to populate the data in table
+var populate = function(inputData) {
+    inputData.forEach(ufo_sightings => {
+      var row = tbody.append("tr");
+      dataList.forEach(index => row.append("td").text(ufo_sightings[index])
+      )
+    });
+  }
+
+// Fetch data from data.js  
+populate(tableData);  
+
 
